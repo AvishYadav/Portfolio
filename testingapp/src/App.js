ce777103125f3a9508e78b0fb5f261166173c5a0
Avish Yadav <84311613+AvishYadav@users.noreply.github.com>
2023-01-07 13:38:38 +0000

    Landing page v1

--- testingapp/src/App.js
+++ testingapp/src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="app">
-      <div className='app_body'>
-        <Sidebar />
-        <Chat />
-      </div>
-    </div>
+    <Home />
   );
 }
 
---
+++ testingapp/src/Bg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Bg.css';
+
+const Bg = () => {
+  return (
+    <div className='bg'>
+      
+    </div>
+  )
+}
+
+export default Bg
