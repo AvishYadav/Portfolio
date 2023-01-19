583db3cbe6399fd62edc46965ec25fea4d793b78
Avish Yadav <84311613+AvishYadav@users.noreply.github.com>
2023-01-19 16:36:10 +0000

    Revert "Adding About page"
    This reverts commit 93ddb3bbc4006f40ee1106dfc1e4887228d0b039.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Landing from './Landing'
-import AboutMe from './AboutMe'
 import './Home.css'
 
 const Home = () => {
   return (
     <div className='home'>
       <Landing />
-      <AboutMe/>
     </div>
   )
 }
